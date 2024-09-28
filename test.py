ae4c1406f686d06c2125e5ea252a9f439e66c095
NfSix <antohagrushin@mail.ru>
2024-09-28 10:36:17 +0300

    finished project

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ import mymod
 print("Testing functions. Part 1")
 print(mymod.mysum(1,2))
 print(mymod.mysub(1,2))
+print("Testing functions. Part 2")
+print(mymod.mydiv(1,2))
+print(mymod.mymul(1,2))
+print("Finished testing")
